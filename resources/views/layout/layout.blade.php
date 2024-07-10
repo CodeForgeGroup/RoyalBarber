03bb67c3bfc8ea29755ff22adb84f0d80a5a12ad
Gustavo Hermogenes <gustavohermogenes1@gmail.com>
2024-07-09 23:15:39 -0300

    feature: visual do app

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-BR">
   <head>
     <title>Royal Barber</title>
     <meta charset="utf-8">
@@ -12,6 +12,11 @@
     <link rel="stylesheet" href="{{ asset ('css/owl.carousel.min.css') }}">
     <link rel="stylesheet" href="{{ asset ('css/magnific-popup.css') }}">
 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
     <link rel="stylesheet" href="{{ asset ('fonts/ionicons/css/ionicons.min.css') }}">
     <link rel="stylesheet" href="{{ asset ('fonts/fontawesome/css/font-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset ('fonts/flaticon/font/flaticon.css') }}">
@@ -23,6 +28,91 @@
     <link rel="shortcut icon" href="{{ asset('images/logo-royalbarberBRANCO-borda.png') }}" type="image/x-icon">
   </head>
   <body>
+
+    <style>
+        /*** Footer ***/
+.footer .btn.btn-social {
+    margin-right: 5px;
+    width: 35px;
+    height: 35px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: var(--light);
+    border: 1px solid #FFFFFF;
+    border-radius: 35px;
+    transition: .3s;
+}
+
+.footer .btn.btn-social:hover {
+    color: var(--primary);
+}
+
+.footer .btn.btn-link {
+    display: block;
+    margin-bottom: 5px;
+    padding: 0;
+    text-align: left;
+    color: #FFFFFF;
+    font-size: 15px;
+    font-weight: normal;
+    text-transform: capitalize;
+    transition: .3s;
+}
+
+.footer .btn.btn-link::before {
+    position: relative;
+    content: "\f105";
+    font-family: "Font Awesome 5 Free";
+    font-weight: 900;
+    margin-right: 10px;
+}
+
+.footer .btn.btn-link:hover {
+    letter-spacing: 1px;
+    box-shadow: none;
+}
+
+.footer .copyright {
+    padding: 25px 0;
+    font-size: 15px;
+    border-top: 1px solid rgba(256, 256, 256, .1);
+}
+
+.footer .copyright a {
+    color: var(--light);
+}
+
+.footer .footer-menu a {
+    margin-right: 15px;
+    padding-right: 15px;
+    border-right: 1px solid rgba(255, 255, 255, .1);
+}
+
+.footer .footer-menu a:last-child {
+    margin-right: 0;
+    padding-right: 0;
+    border-right: none;
+}
+
+.site-footer {
+    position: relative; /* Adicione position: relative para que ::before seja relativo a .site-footer */
+    background-image: url(../images/Gif.webp);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;/* Define a cor do texto para branco, para contraste com o fundo escuro */
+}
+
+.site-footer::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7); /* Escurece a imagem de fundo com 50% de opacidade */
+}
+    </style>
 
     <header role="banner">
 
@@ -82,52 +172,57 @@
 
 
 
-
-    <footer class="site-footer">
-        <div class="container">
-            <div class="row mb-5">
-                <div class="col-md-4">
-                    <h3 class="mb-4">Sobre</h3>
-                    <p class="mb-4">Na nossa barbearia, estamos comprometidos em oferecer serviços de alta qualidade, estilo e atendimento personalizado. Descubra o seu melhor visual conosco</p>
-                    <ul class="list-unstyled ">
-                        <li class="d-flex"><span class="mr-3"><span class="icon ion-ios-location"></span></span><span class="">Avenida Marechal Tito, 1500 - São Paulo, Brasil</span></li>
-                        <li class="d-flex"><span class="mr-3"><span class="icon ion-ios-telephone"></span></span><span class="">11 951694143</span></li>
-                        <li class="d-flex"><span class="mr-3"><span class="icon ion-email"></span></span><span class="">codeforgegroup@gmail.com</span></li>
-                    </ul>
+    <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn site-footer" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Nos conheça</h4>
+                    <a class="btn btn-link" href="{{ url('/') }}">Início</a>
+                    <a class="btn btn-link" href="{{ url('/sobre') }}">Sobre</a>
+                    <a class="btn btn-link" href="{{ url('cortes') }}">Serviços</a>
+                    <a class="btn btn-link" href="{{ url('/contato') }}">Contato</a>
                 </div>
-                <div class="col-md-2">
-                    <h3 class="mb-4">Links</h3>
-                    <ul class="list-unstyled ">
-                        <li><a href="{{ url('/') }}">Início</a></li>
-                        <li><a href="{{ url('/sobre') }}">Sobre</a></li>
-                        <li><a href="{{ url('cortes') }}">Serviços</a></li>
-                        <li><a href="{{ url('/contato') }}">Contato</a></li>
-                    </ul>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Constate-nos</h4>
+                    <p class="mb-2">Avenida Marechal Tito, 1500 - São Paulo, Brasil/p>
+                    <p class="mb-2">(11)95169-04143</p>
+                    <p class="mb-2">codeforgegroup@gmail.com</p>
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                    </div>
                 </div>
-                <div class="col-md-3">
-                    <h3>Conecte-se</h3>
-                    <p>
-                        <a href="#" class="p-2"><span class="fa fa-facebook"></span></a>
-                        <a href="#" class="p-2"><span class="fa fa-twitter"></span></a>
-                        <a href="#" class="p-2"><span class="fa fa-instagram"></span></a>
-                    </p>
-                </div>
-
-                <div class="col-md-3">
-                    <iframe src="https://www.google.com/maps/embed?pb=!4v1710634152608!6m8!1m7!1sul2wxu2vksDYtWPq-8SJXA!2m2!1d-23.55317480376496!2d-46.56291741588109!3f199.5034453586373!4f6.901900927865853!5f1.5154630754046785" width="250" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-7 text-center">
-                    <!-- Link de volta para o Colorlib não pode ser removido. O modelo está licenciado sob CC BY 3.0. -->
-                    Direitos Autorais &copy;<script>document.write(new Date().getFullYear());</script> Todos os direitos reservados por <a href="https://colorlib.com" target="_blank" style="color: orangered">Code Forge</a>
-                    <!-- Link de volta para o Colorlib não pode ser removido. O modelo está licenciado sob CC BY 3.0. -->
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="section-title ff-secondary text-start text-primary fw-normal mb-4">Expediente</h4>
+                    <h5 class="text-light fw-normal">Segunda - Sexta</h5>
+                    <p>10:00 - 20:00</p>
+                    <h5 class="text-light fw-normal">Sábados</h5>
+                    <p>11:00 - 21:00</p>
                 </div>
             </div>
         </div>
-    </footer>
-    <!-- FIM do rodapé -->
-
+        <div class="container">
+            <div class="copyright">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a class="border-bottom" href="#">RoyalBarber</a>, Todos os direitos reservados. 
+                        
+                        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                        Desenvolvido por <a class="border-bottom" href="https://htmlcodex.com">CodeForge</a>
+                    </div>
+                    <div class="col-md-6 text-center text-md-end">
+                        <div class="footer-menu">
+                            <a href="">Cadastre-se</a>
+                            <a href="">App</a>
+                            <a href="">Contato</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
       <!-- loader -->
       <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214"/></svg></div>
